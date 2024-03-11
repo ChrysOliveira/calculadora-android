b2b13ae58aa806a80307ea57583eff7813bf1f59
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2024-03-11 00:34:25 -0300

    primeira versao simples

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.calculadora
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initListeners()
+    }
+
+    fun initListeners(){
+
+        binding.buttonCalcular.setOnClickListener {
+            var resultado = when (binding.radioGroup.checkedRadioButtonId){
+                binding.radioButtonAdicao.id -> adicao()
+                binding.radioButtonSubtracao.id -> subtracao()
+                binding.radioButtonMultiplicacao.id -> multiplicacao()
+                binding.radioButtonDivisao.id -> divisao()
+                else -> Toast.makeText(this, "Selecione uma opcao", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.radioGroup.setOnCheckedChangeListener { _, _ ->
+            binding.textViewResultado.text = "Resultado: "
+            binding.editTextValor1.text = null
+            binding.editTextValor2.text = null
         }
     }
+
+    fun adicao(){
+        var resultado = binding.editTextValor1.text.toString().toFloat() + binding.editTextValor2.text.toString().toFloat()
+
+        binding.textViewResultado.text = resultado.toString()
+    }
+
+    fun subtracao(){
+        var resultado = binding.editTextValor1.text.toString().toFloat() - binding.editTextValor2.text.toString().toFloat()
+
+        binding.textViewResultado.text = resultado.toString()
+    }
+
+    fun multiplicacao(){
+        var resultado = binding.editTextValor1.text.toString().toFloat() * binding.editTextValor2.text.toString().toFloat()
+
+        binding.textViewResultado.text = resultado.toString()
+    }
+
+    fun divisao(){
+        if (binding.editTextValor2.text.toString().toFloat() == 0.0.toFloat()){
+            Toast.makeText(this, "O segundo valor nao pode ser 0.0", Toast.LENGTH_SHORT).show()
+        } else {
+            var resultado = binding.editTextValor1.text.toString().toFloat() / binding.editTextValor2.text.toString().toFloat()
+
+            binding.textViewResultado.text = resultado.toString()
+        }
+    }
+
 }
